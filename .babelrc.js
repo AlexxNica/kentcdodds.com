16c10ebf02026622d607526fdc48747e2e20ad8f
AlexxNica <kryndex@gmail.com>
2017-10-18 10:04:02 -0200

    better config separation of concerns

---
+++ .babelrc.js
@@ -0,0 +1,12 @@
+// @flow
+'use strict';
+
+module.exports = {
+  presets: [
+    'kcd-scripts/babel',
+  ],
+  plugins: [
+    'babel-macros',
+    'react-require',
+  ],
+}
---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+// @flow
+module.exports = {
+  'root': true,
+  //'parser': 'babel-eslint',
+  /*'env': {
+    'browser': true,
+    'node': true,
+    'commonjs': true,
+    'shared-node-browser': true,
+    'worker': true,
+    'jest': true,
+    'serviceworker': true,
+    'es6': true
+  },
+  'parserOptions': {
+    'ecmaVersion': 8,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true,
+      'modules': true,
+      'impliedStrict': true,
+      'experimentalObjectRestSpread': true
+    }
+  },*/
+  'extends': [
+    './node_modules/kcd-scripts/eslint.js',
+    'kentcdodds/babel-react-require',
+  ],
+  'rules': {
+    'react/jsx-max-props-per-line': 'off',
+  }
+};
